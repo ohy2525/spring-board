9cbced3aedd227748de6f90ac52f2e5b3a25fbc6
ohy2525 <dhgodbs2@naver.com>
2023-01-09 16:39:31 +0900

    데이터베이스 접근 테스트

---
+++ spring-board/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into article (title, content, hashtag, created_at, created_by, modified_at, modified_by) values
+('first article', 'first test test test test test test', '#test', '2021-03-10 08:48:50', 'ohy', '2021-05-30 23:53:46', 'ohy');
+
+insert into article_comment (article_id, content, created_at, created_by, modified_at, modified_by) values
+(1, 'reply', '2022-03-10 08:48:50', 'sbb', '2022-05-30 23:53:46', 'sbb');
--- spring-board/src/test/java/com/ohy/springboard/repository/JpaRepositoryTest.java
+++ spring-board/src/test/java/com/ohy/springboard/repository/JpaRepositoryTest.java
@@ -1,11 +1,17 @@
 package com.ohy.springboard.repository;
 
 import com.ohy.springboard.config.JpaConfig;
+import com.ohy.springboard.domain.Article;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
@@ -25,7 +31,57 @@ class JpaRepositoryTest {
 
     @DisplayName("select 테스트")
     @Test
-    void givenTestData_whenSelecting_thenWorksFine() {
+    void findAllTest() {
+        //given
+        //when
+        List<Article> articles = articleRepository.findAll();
 
+        //then
+        assertThat(articles).isNotNull().hasSize(1);
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void insertTest() {
+        //given
+        long previousCount = articleRepository.count();
+
+        //when
+        Article savedArticle = articleRepository.save(Article.of("insert test", "test content", "#spring"));
+
+        //then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void updateTest() {
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updatedHashtag = "#spring boot";
+        article.setHashtag(updatedHashtag);
+
+        //when
+        Article savedArticle = articleRepository.saveAndFlush(article);
+
+        //then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void deleteTest() {
+        //given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();  //cascade 테스트
+        int deletedCommentSize = article.getArticleComments().size();
+
+        //when
+        articleRepository.delete(article);
+
+        //then
+        assertThat(articleRepository.count()).isEqualTo(previousCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentSize);
     }
 }
