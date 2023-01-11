586a236cc4eca326447e0ad3948db42c5bac825c
ohy2525 <dhgodbs2@naver.com>
2023-01-11 11:26:55 +0900

    api 구현
    Queyrdsl
    QuerydslPredicateExecutor
    QuerydslBinderCustomizer

--- spring-board/src/main/java/com/ohy/springboard/repository/ArticleCommentRepository.java
+++ spring-board/src/main/java/com/ohy/springboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,29 @@
 package com.ohy.springboard.repository;
 
 import com.ohy.springboard.domain.ArticleComment;
+import com.ohy.springboard.domain.QArticle;
+import com.ohy.springboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+
+        bindings.excludeUnlistedProperties(true); // 모든 필드 검색 허용하지 않을 때. 기본값 false
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    }
+
 }
--- spring-board/src/main/java/com/ohy/springboard/repository/ArticleRepository.java
+++ spring-board/src/main/java/com/ohy/springboard/repository/ArticleRepository.java
@@ -1,9 +1,32 @@
 package com.ohy.springboard.repository;
 
 import com.ohy.springboard.domain.Article;
+import com.ohy.springboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,   // Article 모든 필드에 대한 기본 검색기능 추가
+        QuerydslBinderCustomizer<QArticle> {
+    @Override //검색 세부구현
+    default void customize(QuerydslBindings bindings, QArticle root) {
+
+        bindings.excludeUnlistedProperties(true); // 모든 필드 검색 허용하지 않을 때. 기본값 false
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);  // like '%${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    }
+
 }
