ca6668dbdfc6637fa9f85776cc87a7515fc60646
ohy2525 <dhgodbs2@naver.com>
2023-01-11 10:26:16 +0900

    data rest 테스트

--- spring-board/src/test/java/com/ohy/springboard/controller/DataRestTest.java
+++ spring-board/src/test/java/com/ohy/springboard/controller/DataRestTest.java
@@ -1,16 +1,20 @@
 package com.ohy.springboard.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DisplayName("Data Rest - API 테스트")
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 public class DataRestTest {
@@ -21,12 +25,57 @@ public class DataRestTest {
         this.mvc = mvc;
     }
 
+    @DisplayName("게시글 리스트 조회")
     @Test
-    void test() throws Exception {
+    void RequestArticles() throws Exception {
         //given
 
         //when & then
         mvc.perform((get("/api/articles")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("게시글 단건 조회")
+    @Test
+    void RequestArticle() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform((get("/api/articles/1")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("게시글 댓글 리스트 조회")
+    @Test
+    void RequestArticleArticleComments() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform((get("/api/articles/1/articleComments")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("댓글 리스트 조회")
+    @Test
+    void RequestArticleComments() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform((get("/api/articleComments/1")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("댓글 단건 조회")
+    @Test
+    void RequestArticleComment() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform((get("/api/articleComments")))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
     }
