4fc2a38753c09fd594e19fc4bb0e26b48728761f
ohy2525 <dhgodbs2@naver.com>
2023-01-13 00:01:23 +0900

    Dto 수정

--- spring-board/src/main/java/com/ohy/springboard/dto/ArticleCommentDto.java
+++ spring-board/src/main/java/com/ohy/springboard/dto/ArticleCommentDto.java
@@ -1,5 +1,8 @@
 package com.ohy.springboard.dto;
 
+import com.ohy.springboard.domain.Article;
+import com.ohy.springboard.domain.ArticleComment;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -13,16 +16,30 @@ public record ArticleCommentDto(
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
-    public static ArticleCommentDto of(
-            Long id,
-            Long articleId,
-            UserAccountDto userAccountDto,
-            String content,
-            LocalDateTime createdAt,
-            String createdBy,
-            LocalDateTime modifiedAt,
-            String modifiedBy
-    ) {
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt,
+                                       String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
         return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
     }
+
+    public static ArticleCommentDto from(ArticleComment entity) {
+        return new ArticleCommentDto(
+                entity.getId(),
+                entity.getArticle().getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getContent(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public ArticleComment toEntity(Article entity) {
+        return ArticleComment.of(
+                entity,
+                userAccountDto.toEntity(),
+                content
+        );
+    }
+    
 }
