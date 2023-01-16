0fcbd9b58cde2c99b06f420179cb83ca42a6fe76
ohy2525 <dhgodbs2@naver.com>
2023-01-16 15:19:24 +0900

    타임리프 decoupled-logic 오류 수정

--- spring-board/src/main/java/com/ohy/springboard/config/ThymeleafConfig.java
+++ spring-board/src/main/java/com/ohy/springboard/config/ThymeleafConfig.java
@@ -5,16 +5,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
-//타임리프 DecoupledLogic을 사용하기 위한 config
+@Configuration
 public class ThymeleafConfig {
 
     @Bean
-    public SpringResourceTemplateResolver thymeleafTemplateResolver(SpringResourceTemplateResolver defaultTemplateResolver, Thymeleaf3Properties thymeleaf3Properties) {
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties
+    ) {
         defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+
         return defaultTemplateResolver;
     }
+
 
     @RequiredArgsConstructor
     @Getter
